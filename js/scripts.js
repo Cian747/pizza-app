aa5aa8f4de01df82e734b482e14e65abb76b24d4
Cian <cian.omondi@student.moringaschool.com>
2021-06-24 22:21:19 +0300

    added UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,23 @@
 //business logic
-
+function Pizza(first,pizza,size,toppings,crust,delivery){
+    this.firstName = first;
+    this.pizzaType = pizza;
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.deliveryMethod = delivery;
+    this.order = function(){
+      console.log(first + " the order will be delivered at your doorstep. Thanks for choosing us!")
+    };
+  }
 
 
 // user interface
 
 $(document).ready(function(){
     $("#add-order").click(function(){
-        $('#new-order').append('<div class="pizza-type">'+
+        $('#new-order').append(''+
+                            '<div class="pizza-type">'+
                                 '<h4>Pizza type:</h4>' +
                                     '<select class="form-control">' +
                                         '<option>Haiwaiian</option>' +
@@ -47,5 +58,50 @@ $(document).ready(function(){
                                     '</div>')
     });
 
-    $("#")
+    $("form#order").submit(function(){
+        event.preventDefault();
+        var inputedFirstName = $('#new-first-name').val(),
+            pizzaType = $('#p-type').val(),
+            yourSize = $("input[name ='pizza-size']:checked").val(),
+            yourToppings = $('#top').val(),
+            theCrust = $('#crust').val(),
+            yourDel = $("#delivery-method").val(),
+            newPizza = new Pizza(inputedFirstName,pizzaType,yourSize,yourToppings,theCrust,yourDel);
+            console.log(newPizza);
+
+            var total = 0;
+
+
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Large" || theCrust == "Crispy"){
+        total = 1200;    
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Medium" || theCrust == "Crispy"){
+        total = 950;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Small" || theCrust == "Crispy"){
+        total = 750;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Large" || theCrust == "Stuffed"){
+        total = 1500; 
+    }       
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Medium" || theCrust == "Stuffed"){
+        total = 1000;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Small" || theCrust == "Stuffed"){
+        total = 950;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Large" || theCrust == "Gluten free"){
+        total = 1500;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Medium" || theCrust == "Gluten free"){
+        total = 1000;
+    }
+    if(pizzaType == "Hawaiian" || pizzaType == "Pepporoni" || pizzaType == "Meat Deluxe" || pizzaType == "Italian Fiesta (Fan favorite)" || pizzaType == "Vegeterian" || yourSize == "Small" || theCrust == "Gluten free"){
+        total = 950;
+    }
+    if(yourToppings == "Extra meat" || yourToppings == "Extra veggies" || yourToppings == "Extra cheese"){
+        total = total + 300;
+    }
+    alert(total);
+    });
 });
